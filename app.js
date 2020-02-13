f3d0c3b1bee93a907bb610c8528177b321e877ec
is0ly <iljaosipov@gmail.com>
2020-02-13 10:11:40 +0300

    added return object of entries

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const randomArray = (len, value) => {
   for (let i = 0; i < len; i += 1) {
     array.push(Math.round(Math.random() * (value - 1)));
   }
-  return array;
+  return array.reduce((totally, number) => {
+    const all = totally;
+    all[number] = (totally[number] || 0) + 1;
+    return all;
+  }, {});
 };
+console.log(randomArray(5, 5));
 
 module.exports = { randomArray };
